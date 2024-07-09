53a8bd56ea3be3a4256e7aaefa9c7306ec6a1252
ShortMikelon <kenesaset485@gmail.com>
2024-07-09 15:43:33 +0500

    Initial commit

---
+++ src/main/java/kz/asetkenes/solidbankapp/SolidBankAppApplication.java
@@ -0,0 +1,13 @@
+package kz.asetkenes.solidbankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidBankAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/asetkenes/solidbankapp/SolidbankappApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.asetkenes.solidbankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidbankappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

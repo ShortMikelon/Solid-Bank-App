ccf550ce8f72c8e0d84510a0df8d3027b006208b
ShortMikelon <kenesaset485@gmail.com>
2024-07-09 15:49:10 +0500

    Add: CreateAccountOperationUi

---
+++ src/main/java/kz/asetkenes/solidbankapp/ui/CreateAccountOperationUi.java
@@ -0,0 +1,10 @@
+package kz.asetkenes.solidbankapp.ui;
+
+import kz.asetkenes.solidbankapp.domain.entities.AccountType;
+
+public interface CreateAccountOperationUi {
+
+    AccountType requestAccountType();
+
+}
+

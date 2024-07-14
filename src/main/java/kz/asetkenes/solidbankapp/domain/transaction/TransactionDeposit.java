4abd6a73dc25efaf86735c21ba8326213f05c9e1
ShortMikelon <kenesaset485@gmail.com>
2024-07-14 17:44:48 +0500

    Added TransactionWithdraw and TransactionDeposit

---
+++ src/main/java/kz/asetkenes/solidbankapp/domain/transaction/TransactionDeposit.java
@@ -0,0 +1,39 @@
+package kz.asetkenes.solidbankapp.domain.transaction;
+
+import kz.asetkenes.solidbankapp.data.transactions.TransactionDao;
+import kz.asetkenes.solidbankapp.domain.entities.AccountWithdraw;
+import kz.asetkenes.solidbankapp.domain.transaction.entities.Transaction;
+import kz.asetkenes.solidbankapp.domain.transaction.entities.TransactionType;
+import kz.asetkenes.solidbankapp.services.account.AccountDepositService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class TransactionDeposit {
+
+    private final TransactionDao transactionDao;
+
+    private final AccountDepositService accountDepositService;
+
+    @Autowired
+    public TransactionDeposit(AccountDepositService accountDepositService, TransactionDao transactionDao) {
+        this.accountDepositService = accountDepositService;
+        this.transactionDao = transactionDao;
+    }
+
+    public void execute(AccountWithdraw account, double amount) {
+        accountDepositService.deposit(account, amount);
+
+        Transaction newTransaction = new Transaction(
+                TransactionType.DEPOSIT,
+                account.getId(),
+                account.getClientId(),
+                amount,
+                System.currentTimeMillis()
+        );
+
+        transactionDao.addTransaction(newTransaction);
+    }
+}
+
---
+++ src/main/java/kz/asetkenes/solidbankapp/domain/transaction/TransactionWithdraw.java
@@ -0,0 +1,37 @@
+package kz.asetkenes.solidbankapp.domain.transaction;
+
+import kz.asetkenes.solidbankapp.data.transactions.TransactionDao;
+import kz.asetkenes.solidbankapp.domain.entities.AccountWithdraw;
+import kz.asetkenes.solidbankapp.domain.transaction.entities.Transaction;
+import kz.asetkenes.solidbankapp.domain.transaction.entities.TransactionType;
+import kz.asetkenes.solidbankapp.services.account.AccountWithdrawService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionWithdraw {
+
+    private final AccountWithdrawService accountWithdrawService;
+
+    private final TransactionDao transactionDao;
+
+    public TransactionWithdraw(
+            AccountWithdrawService accountWithdrawService,
+            TransactionDao transactionDao
+    ) {
+        this.accountWithdrawService = accountWithdrawService;
+        this.transactionDao = transactionDao;
+    }
+
+    public void execute(AccountWithdraw account, double amount) {
+        accountWithdrawService.withdraw(amount, account);
+        Transaction transaction = new Transaction(
+                TransactionType.WITHDRAW,
+                account.getId(),
+                account.getClientId(),
+                amount,
+                System.currentTimeMillis()
+        );
+
+        transactionDao.addTransaction(transaction);
+    }
+}

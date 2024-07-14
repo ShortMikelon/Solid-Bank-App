5c98eddeb5b6d270bb454a982a9e7269374a7c5c
ShortMikelon <kenesaset485@gmail.com>
2024-07-14 17:40:29 +0500

    changed ApplicationContext to AnnotationConfigApplicationContext

--- src/main/java/kz/asetkenes/solidbankapp/SolidBankApp.java
+++ src/main/java/kz/asetkenes/solidbankapp/SolidBankApp.java
@@ -3,17 +3,18 @@ package kz.asetkenes.solidbankapp;
 import kz.asetkenes.solidbankapp.ui.AccountBasicCli;
 import kz.asetkenes.solidbankapp.ui.MyCli;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
 
 public class SolidBankApp {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("props.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         String clientId = "1";
 
-        AccountBasicCli basicCli = (AccountBasicCli) context.getBean("accountBasicCli");
-        MyCli myCli = (MyCli) context.getBean("myCli");
+        AccountBasicCli basicCli = context.getBean(AccountBasicCli.class);
+        MyCli myCli = context.getBean(MyCli.class);
 
         String helpMessage = """
 				1 - show accounts
@@ -32,36 +33,17 @@ public class SolidBankApp {
             String inputOperationNumber = myCli.getScanner().nextLine();
 
             switch (inputOperationNumber) {
-                case "1": {
-                    basicCli.getAccounts(clientId);
-                    break;
-                }
-                case "2": {
-                    basicCli.createAccountRequest(clientId);
-                    break;
-                }
-                case "3": {
-                    System.out.println("Not yet implementation");
-                    break;
-                }
-                case "4": {
-                    System.out.println("Not yet implementation");
-                    break;
-                }
-                case "5": {
-                    System.out.println("Not yet implementation");
-                    break;
-                }
-                case "6": {
-                    System.out.println(helpMessage);
-                    break;
-                }
-                case "7": {
+                case "1" -> basicCli.getAccounts(clientId);
+                case "2" -> basicCli.createAccountRequest(clientId);
+                case "3" -> System.out.println("Not yet implementation");
+                case "4" -> System.out.println("Not yet implementation");
+                case "5" -> System.out.println("Not yet implementation");
+                case "6" -> System.out.println(helpMessage);
+                case "7" -> {
                     System.out.println("Application closed");
                     isFinished = true;
-                    break;
                 }
-                default: System.out.println("Incorrect number operation. Please Again!");
+                default -> System.out.println("Incorrect number operation. Please Again!");
             }
         }
     }

a3f203e4f5899f5fbae349ce268c3c8604f5f84c
ShortMikelon <kenesaset485@gmail.com>
2024-07-14 17:44:04 +0500

    Added AccountDepositService and its implementation

---
+++ src/main/java/kz/asetkenes/solidbankapp/services/account/impl/AccountDepositServiceImpl.java
@@ -0,0 +1,14 @@
+package kz.asetkenes.solidbankapp.services.account.impl;
+
+import kz.asetkenes.solidbankapp.domain.entities.AccountWithdraw;
+import kz.asetkenes.solidbankapp.services.account.AccountDepositService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountDepositServiceImpl implements AccountDepositService {
+
+    @Override
+    public void deposit(AccountWithdraw account, double amount) {
+
+    }
+}
